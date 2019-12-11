a4d51d9df9610834f6342990cd220285ea1cbb65
David Milanese <milanese.david@gmail.com>
2019-12-11 11:29:24 +1100

    install rspec benchmark

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "bundler/setup"
 require "lazy_sort_by"
 require "enumerable"
+require "rspec-benchmark"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -12,4 +13,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include RSpec::Benchmark::Matchers  
 end
