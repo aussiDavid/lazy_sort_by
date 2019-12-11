a280ab07773f910f703400f03909a5afb83d6647
David Milanese <milanese.david@gmail.com>
2019-12-11 11:29:49 +1100

    implement lazy sorting for up to 2 items in a list

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,9 +1,27 @@
 module Enumerable
-  def lazy_sort_by(&block)
-    if block
-      sort_by(&block)
-    else
-      sort
+  def lazy_sort_by &block
+    if size == 1
+      return self
+    end
+    
+    if size == 2
+      block.call.each do |compariator|
+        if compariator.(first) > compariator.(last)
+          return [last, first]
+        end
+        
+        if compariator.(first) < compariator.(last)
+          return [first, last]
+        end
+      end
+      
+      return [first, last]
+    end
+
+    if size == 3
+      return block.call.reduce(first(2)) do |compariator, acc|
+        compariator.(acc[0]) > compariator.(acc[1]) ? [acc[1], acc[0]] : [acc[0], acc[1]]
+      end
     end
   end
 end
--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,11 +2,82 @@ RSpec.describe Enumerable do
   it { expect([]).to respond_to :lazy_sort_by }
 
   describe '#lazy_sort_by' do
-    it { expect([].lazy_sort_by).to eq([]) }
-    it { expect([1].lazy_sort_by).to eq([1]) }
-    it { expect([1, 2].lazy_sort_by).to eq([1, 2]) }
-    it { expect([2, 1].lazy_sort_by).to eq([1, 2]) }
-    it { expect([{ a: 2 }, { a: 1 }].lazy_sort_by { |x| x[:a] }).to eq([{ a: 1 }, { a: 2 }]) }
-    it { expect([{ a: 2 }, { a: 1 }].lazy_sort_by { |x| [x[:a]] }).to eq([{ a: 1 }, { a: 2 }]) }
+    context 'when sorting 1 item' do
+      subject(:lazy_sort_by) { [one].lazy_sort_by { [ ->(x) { x.length }, ->(x) { x.size } ] } }
+        
+      let(:one) { double :one, length: 1, size: 1 }
+    
+      it { is_expected.to eq [one] }
+    end
+    
+    context 'when sorting 2 items' do
+      subject(:lazy_sort_by) { [two, one].lazy_sort_by { [ ->(x) { x.length }, ->(x) { x.size } ] } }
+        
+      let(:one) { double :one, length: 1, size: 2 }
+      let(:two) { double :two, length: 3, size: 4 }
+    
+      it { is_expected.to eq [one, two] }
+    end
+    
+    context 'when sorting 2 items with an expensive second term' do
+      subject(:lazy_sort_by) do
+        [two, one].lazy_sort_by do
+          [
+            ->(x) { x.length },
+            ->(x) { sleep(3); x.size }
+          ]
+        end
+      end
+        
+      let(:one) { double :one, length: 1, size: 2 }
+      let(:two) { double :two, length: 3, size: 4 }
+    
+      it { expect { lazy_sort_by }.to perform_under(3).sec }
+      it { is_expected.to eq [one, two] }
+    end
+    
+    context 'when sorting 2 items with the same first terms' do
+      subject(:lazy_sort_by) { [two, one].lazy_sort_by { [ ->(x) { x.length }, ->(x) { x.size } ] } }
+        
+      let(:one) { double :one, length: 1, size: 2 }
+      let(:two) { double :two, length: 1, size: 3 }
+    
+      it { is_expected.to eq [one, two] }
+    end
+    
+    context 'when sorting 2 items with the same first of 3 terms' do
+      subject(:lazy_sort_by) do
+        [two, one].lazy_sort_by do
+          [
+            ->(x) { x.length },
+            ->(x) { x.cars },
+            ->(x) { x.size } 
+          ]
+        end
+      end
+        
+      let(:one) { double :one, length: 1, cars: 2, size: 2 }
+      let(:two) { double :two, length: 1, cars: 2, size: 3 }
+    
+      it { is_expected.to eq [one, two] }
+    end
+
+    context 'when sorting 3 items with similar first of 3 terms' do
+      subject(:lazy_sort_by) do
+        [three, two, one].lazy_sort_by do
+          [
+            ->(x) { x.length },
+            ->(x) { x.cars },
+            ->(x) { x.size } 
+          ]
+        end
+      end
+        
+      let(:one) { double :one, length: 1, cars: 2, size: 2 }
+      let(:two) { double :two, length: 1, cars: 2, size: 3 }
+      let(:three) { double :three, length: 1, cars: 2, size: 4 }
+    
+      it { is_expected.to eq [one, two, three] }
+    end
   end
 end
